6900b7277e3c035577e49b40521d80f2b5799092
harishnarasimhan <89064571+harishnarasimhan@users.noreply.github.com>
2021-08-18 12:55:24 +0530

    Add files via upload

---
+++ harish.js
@@ -0,0 +1,13 @@
+function putBlue(){
+    document.body.style.backgroundColor="Blue";
+}
+
+function putViolet(){
+    document.body.style.backgroundColor="Violet";
+}
+function putGrey(){
+    document.body.style.backgroundColor="Grey";
+}
+function putOrange(){
+    document.body.style.backgroundColor="Orange";
+}
